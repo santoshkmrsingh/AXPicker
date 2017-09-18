1d170d96a757eca06dd88904b1ab2e34d8bd6897
Santosh kumar singh <santoshkmrsingh@hotmail.com>
2017-09-18 17:37:59 +0400

    Revert "conflict resolution"
    This reverts commit a558668cad17be2ebdc1d72ef470aacc533798c3.
    
    stuck#

--- src/pages/data-area/data-area.ts
+++ src/pages/data-area/data-area.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-<<<<<<< Updated upstream
 import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { AxServiceProvider } from '../../providers/ax-service/ax-service';
 import { StartPage } from '../start/start';
-=======
-import { NavController, NavParams } from 'ionic-angular';
-import { AxServiceProvider } from '../../providers/ax-service/ax-service';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'page-data-area',
@@ -17,12 +12,8 @@ export class DataAreaPage {
   public companies: any;
   public company: any;
 
-<<<<<<< Updated upstream
   constructor(public navCtrl: NavController, public navParams: NavParams, 
     public axServiceProvider: AxServiceProvider, public loadingCtrl: LoadingController) {    
-=======
-  constructor(public navCtrl: NavController, public navParams: NavParams, public axServiceProvider: AxServiceProvider) {    
->>>>>>> Stashed changes
   }
 
   ionViewDidLoad() {
@@ -32,10 +23,7 @@ export class DataAreaPage {
     loading.present();
     console.log('ionViewDidLoad DataAreaPage');
     this.axServiceProvider.getCompanyList().subscribe((data) => {
-<<<<<<< Updated upstream
       loading.dismiss();
-=======
->>>>>>> Stashed changes
       this.companies = data;
     })
   }
